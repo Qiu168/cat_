06fccd795304042f70948551940e59613be5cb40
Qiu168 <1462909477@qq.com>
2023-03-27 17:11:31 +0800

    feat(dao,resources,config):数据库连接池

---
+++ src/main/java/com/HuangTaiQi/www/utils/ConnectionPoolManager.java
@@ -0,0 +1,38 @@
+package com.HuangTaiQi.www.utils;
+
+import com.HuangTaiQi.www.config.DataSourceConfig;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author 14629
+ */
+public class ConnectionPoolManager {
+
+    private static DataSourceConfig config = new DataSourceConfig();
+    private static ImplConnectionPool connectionPool;
+    private static Logger logger = Logger.getLogger("com.HuangTaiQi.www.utils.ConnectionPoolManager");
+    static {
+        try {
+            connectionPool = new ImplConnectionPool(config);
+        } catch (SQLException | ClassNotFoundException e) {
+            logger.log(Level.SEVERE,"mes,e={0}",e.getMessage());
+        }
+    }
+
+    public static Connection getConnection() throws InterruptedException, SQLException {
+        return connectionPool.getPoolEntry().getConn();
+    }
+
+    public static void closeConnection(Connection connection) throws SQLException {
+        connectionPool.close(connection);
+    }
+
+    public static void closeAll(Connection connection, Statement statement, ResultSet resultSet) throws SQLException {connectionPool.closeAll(connection,statement,resultSet);}
+
+}
