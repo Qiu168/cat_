8a4fb53ce79354d24f93d0d0e5f9b7c8303c4348
Qiu168 <1462909477@qq.com>
2023-04-04 23:46:42 +0800

    style(service):加了注释

--- src/main/java/com/HuangTaiQi/www/service/CommentService.java
+++ src/main/java/com/HuangTaiQi/www/service/CommentService.java
@@ -5,8 +5,25 @@ import com.HuangTaiQi.www.po.CommentEntity;
 import java.sql.SQLException;
 import java.util.List;
 
+/**
+ * comment
+ * @author 14629
+ */
 public interface CommentService {
+    /**
+     * 获取充电桩的评论
+     * @param pileId 充电桩的id
+     * @return 评论集合
+     * @throws Exception 异常
+     */
     List<CommentEntity> getComments(int pileId) throws Exception;
+
+    /**
+     *新增评论
+     * @param pileId 充电桩的id
+     * @param comm 评论
+     * @throws SQLException 异常
+     */
 
     void addComment(int pileId, String comm) throws SQLException;
 }
