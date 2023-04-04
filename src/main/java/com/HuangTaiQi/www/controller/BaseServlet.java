c020719dafdf7e4dc860942db25b76942427d3b5
Qiu168 <1462909477@qq.com>
2023-04-04 22:57:16 +0800

    feat(controller):新增更具不同的异常执行不同逻辑

--- src/main/java/com/HuangTaiQi/www/controller/BaseServlet.java
+++ src/main/java/com/HuangTaiQi/www/controller/BaseServlet.java
@@ -1,6 +1,9 @@
 package com.HuangTaiQi.www.controller;
 
 
+
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -9,9 +12,24 @@ import java.util.logging.Logger;
  * 获取域名最后的方法名，通过反射调用方法
  */
 public class BaseServlet  {
-    protected void handleException(Class cls,Exception e) {
+    protected <T> void handleException(Class<T> cls, Exception e){
         Logger logger=Logger.getLogger(cls.getName());
-        logger.log(Level.WARNING,e.toString());
-        // 其他处理逻辑
+        try {
+            // 判断异常类型
+            if (e instanceof SQLException) {
+                if (e.getMessage().contains("doesn't exist")) {
+                    logger.log(Level.WARNING,"file not find An error occurred in "+cls.getName()+" e = {0}", e.getMessage());
+                }
+            } else if (e instanceof IOException) {
+                // 处理 IO 异常
+                logger.log(Level.WARNING,"An error occurred in "+cls.getName()+" e = {0}", e.getMessage());
+            } else {
+                logger.log(Level.WARNING,"An error occurred in "+cls.getName()+" e = {0}", e.getMessage());
+            }
+        } catch (Exception ex) {
+            // 处理异常时出现异常
+            logger.log(Level.WARNING,"An error occurred in "+cls.getName()+" e = {0}", e.getMessage());
+        }
     }
+
 }
