aa9fb8fb5c3f98c5ccb687fae6ea36312fd2cfbd
Qiu168 <1462909477@qq.com>
2023-04-04 22:56:08 +0800

    feat(controller):新增commentServlet

---
+++ src/main/java/com/HuangTaiQi/www/controller/CommentServlet.java
@@ -0,0 +1,31 @@
+package com.HuangTaiQi.www.controller;
+
+import com.HuangTaiQi.www.po.CommentEntity;
+import com.HuangTaiQi.www.service.CommentService;
+import com.HuangTaiQi.www.service.impl.CommentServiceImpl;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * @author 14629
+ */
+public class CommentServlet extends BaseServlet{
+    CommentService commentService=new CommentServiceImpl();
+    public List<CommentEntity> getComments(int pileId) {
+        try {
+            return commentService.getComments(pileId);
+        } catch (Exception e) {
+            handleException(CommentServlet.class,e);
+        }
+        return null;
+    }
+
+    public void addComment(int pileId, String comm) {
+        try {
+            commentService.addComment(pileId,comm);
+        } catch (SQLException e) {
+            handleException(CommentServlet.class,e);
+        }
+    }
+}
