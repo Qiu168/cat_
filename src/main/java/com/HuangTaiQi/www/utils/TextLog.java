12e09a0453cd242930c0ad519e92febc5bc002df
Qiu168 <1462909477@qq.com>
2023-04-04 20:03:36 +0800

    feat(TextLog):新增读取文件方法

--- src/main/java/com/HuangTaiQi/www/utils/TextLog.java
+++ src/main/java/com/HuangTaiQi/www/utils/TextLog.java
@@ -1,15 +1,15 @@
 package com.HuangTaiQi.www.utils;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.time.LocalDateTime;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TextLog {
     private static String textLog = "";
-
+    private static final Logger logger=Logger.getLogger(TextLog.class.getName());
     /**
      *  每天的毫秒数
      */
@@ -24,11 +24,17 @@ public class TextLog {
             LocalDateTime now = LocalDateTime.now();
             String fileName = "textLog_" + now.getYear() + "-" + now.getMonthValue() + "-" + now.getDayOfMonth() + ".txt";
             File file = new File(fileName);
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                logger.log(Level.SEVERE,"Failed to create a file");
+                throw new RuntimeException(e);
+            }
             try (FileWriter writer = new FileWriter(file)) {
                 writer.write(textLog);
                 textLog = "";
             } catch (IOException e) {
-                System.err.println("Failed to save text log to file: " + e.getMessage());
+                logger.log(Level.SEVERE,"Failed to save text log to file: " + e.getMessage());
             }
         }
     };
@@ -43,7 +49,32 @@ public class TextLog {
     }
 
     public static void add(String text) {
-        textLog += text;
+        LocalDateTime now = LocalDateTime.now();
+        textLog += "\n"+now.getYear()+":"+now.getMonthValue()+":"+now.getDayOfMonth()+
+                "  "+now.getHour()+":"+now.getMinute()+"\n"+text;
     }
+    public static void readTxt(int year,int month,int day) throws Exception {
+        FileReader fileReader = null;
+        if(TextLog.class.getResource("textLog_" + year + "-" + month+ "-" +day + ".txt")==null){
+            System.out.println("文件找不到，可能没有生成,或你的日期输入错误");
+            return;
+        }
+        File file=new File(TextLog.class.getResource("textLog_" + year + "-" + month+ "-" +day + ".txt").getFile());
+        if(file.exists()){
+            //创建一个读取文件的流对象
+            fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            //定义一个空字符串
+            String str;
+            while((str = bufferedReader.readLine())!= null) {
+                System.out.println(str);
+            }
+            fileReader.close();
+        }else {
+            System.out.println("文件找不到，可能没有生成");
+        }
+    }
+
+
 }
 
